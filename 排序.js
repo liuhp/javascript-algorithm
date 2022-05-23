031f12ac963ebe33f03cfbc6129f2c941298f906
liuhaiping <2190098961@qq.com>
2022-05-23 23:47:12 +0800

    feat: 快排

--- 排序.js
+++ 排序.js
@@ -1,3 +1,42 @@
+// 快速排序
+// 1.将数组根据中介值进行分区，小于于中介值的放中介值的左边，大于中介值的放在中介值右边；
+// 2.返回分区后中介值的索引
+var partition = function(arr, start, end){
+    const len = arr.length
+    let left = 0
+    let right = len - 1
+    while(1){
+        while(left <= end && arr[left] <= arr[start]){ left += 1}
+        while(right >= start + 1 && arr[right] >= arr[start]){ right -= 1}
+        if(left >= right){
+            break
+        }
+        [arr[left], arr[right]]  =[arr[right], arr[left]]
+        left += 1
+        right -= 1
+    }
+    [arr[right], arr[start]] = [arr[start], arr[right]]
+    return right
+}
+
+var quickSort = function(arr, start, end){
+    if(start === end){
+        return
+    }
+    const index = partition(arr, start, end)
+    if(index > start){
+        quickSort(arr, start, index-1)
+    }
+    if(index <end){
+        quickSort(arr, index+1, end)
+    }
+}
+
+// let arr = [3,4,6,8,9]
+// quickSort(arr, 0, 4)
+// console.log(arr)
+
+
 // 选择排序
 function selectionSort(arr) {
     var len = arr.length;
